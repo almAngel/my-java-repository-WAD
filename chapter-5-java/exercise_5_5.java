803de954799139f126e3ee901ad9d17a78461374
AngelLopezMolina <alm.angel97@gmail.com>
2016-10-26 09:44:41 +0200

    Added Ch.5 exercise 5

--- chapter-5-java/exercise_5_5.java
+++ chapter-5-java/exercise_5_5.java
@@ -8,9 +8,13 @@
 public class exercise_5_5 {
   public static void main (String[]args){
   
-  int number = 0;
+  int number = 320;
   
-  
+    while (number >= 160){
+    
+      System.out.println(number);
+      number -= 20;
+    }
   
   }  
 }
