2d19ee84808428b9619a4454cc917ea7c876219f
AngelLopezMolina <alm.angel97@gmail.com>
2016-11-08 10:52:50 +0100

    Fixed ex.18

--- chapter-5-java/exercise_18_5.java
+++ chapter-5-java/exercise_18_5.java
@@ -13,29 +13,13 @@ public class exercise_18_5 {
     System.out.println("Introduce the second number: ");
     int number2= Integer.parseInt(s.nextLine());
     System.out.println("");//Blank.
-    int aux1= 0;
-    int aux2= 0;
     boolean repeat= true;
     
     while(repeat){
       for (int i = number1; i <= number2; i+=7){
-        aux1= i;
-        aux2= aux1;
-        System.out.println(i+"+"+"7= "+(aux1));
-      
-        if (aux2 < number2){
-          System.out.println("You couldn't reach "+number2+" per "+ (number2-aux2) +" numbers.");
-          repeat= false;
-        }
-        else if (aux2 == number2){
-          System.out.println("Success!");
-          i= number2;
-          repeat= false;
-        }
-        else if (aux2 > number2){
-          System.out.println("You exceeded "+number2+" per "+(aux2-number2)+" numbers.");
-          repeat= false;
-        } 
+
+        System.out.print(i+" ");
+        repeat=false;
       } 
     }
 
